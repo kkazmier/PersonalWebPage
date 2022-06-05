550c95228ddfeda869ab56ab1698c201301704a5
kkazmier <poczta.karol.kazmierczak@gmail.com>
2022-06-05 16:54:47 +0200

    Add web visitor counter.

--- index.php
+++ index.php
@@ -20,6 +20,7 @@
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	</head>
     <body id="page-top">
+		
         <?php include "navbar.html"; ?>
         <?php include "masthead.html"; ?>
         <?php include "projectsmain.html"; ?>
@@ -48,6 +49,7 @@
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
+	<script type="text/javascript" src="https://www.free-counters.org/count/av30"></script><br>
+				<a href='https://www.versicherungen.at/pflegeversicherung-rechner/'>Pflegegeld Versicherung</a> <script type='text/javascript' src='https://whomania.com/ctr?id=621ce77e279165d77fb61119daa98481629fff08'></script>
     </body>
 </html>
