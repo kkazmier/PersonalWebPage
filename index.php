a08886b11e19aefc0d7179f0cf7efcb27f58e892
Karol <55816545+kkazmier@users.noreply.github.com>
2022-07-05 22:59:14 +0200

    Adjust to teft.

--- index.php
+++ index.php
@@ -31,14 +31,10 @@
 		<?php include "springprojects.html"; ?>
 		<?php include "javafxprojects.html"; ?>
 		<?php include "technologies.html"; ?>
-        <?php include "about.html"; ?>
-		
-		
-		
+        <?php include "about.html"; ?>		
         <?php include "contact.html"; ?>
-        <?php include "footer.html"; ?>
-        <<?php include "copyright.html"; ?>
-        
+        <?php include "copyright.html"; ?>
+        <?php include "visitorscounter.html"; ?>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
@@ -51,6 +47,6 @@
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<?php include "visitorscounter.html"; ?>
+	
     </body>
 </html>
